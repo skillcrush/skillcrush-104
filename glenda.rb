b5ca2227b61473cc123fa9d00e69100327629372
Glenda Lewis <glewis@cteresource.org>
2015-10-05 11:09:44 -0400

    practicing forking

---
+++ glenda.rb
@@ -0,0 +1,8 @@
+puts "This is my first clone/push exercise."
+puts "Do you think this is fun? (y or n)"
+	answer = gets.downcase
+		if answer == y
+			puts "Yeah, I'm a crazy tech geek!"
+		else puts "This is too hard!"
+		end
+end
