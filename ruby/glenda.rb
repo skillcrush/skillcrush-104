b6970ebcb5710a3e2696135d43b3b4aed9dc771e
Glenda <glenda.c.lewis@gmail.com>
2015-10-09 21:26:40 -0400

    fix advice errors

--- ruby/glenda.rb
+++ ruby/glenda.rb
@@ -1,6 +1,6 @@
-puts "Doyou think this is fun? (y or n)"
+puts "Do you think this is fun? (y or n)"
 answer = gets.chomp
 	if (answer == "y")
 	puts "Yeah, I'm a crazy tech geek!"
-	elsif puts "This is too hard!"
+	else puts "This is too hard!"
 	end
