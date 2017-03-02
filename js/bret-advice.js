043adb20054e93fcd4a1274da53034a7ac9d9782
bretmavrich <bretmavrich@gmail.com>
2017-03-02 16:28:29 -0500

    Trying to get the javascript to work

---
+++ js/bret-advice.js
@@ -0,0 +1,15 @@
+var advicePick = $("#advice-pick").val();
+
+$(document).ready(function(){
+
+
+    $("select").click(function(){
+        $("#instructions").addClass("gone");
+
+    });
+    
+
+
+  
+    
+});
